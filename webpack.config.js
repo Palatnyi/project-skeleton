2e3cf5bce308981cfc22683f5f5e81f868a493e5
andrey <andrey@andreys-MacBook-Pro.local>
2017-05-26 11:39:50 +0300

    added missed loaders to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,60 @@
-const webpack  = require("webpack");
+const webpack = require("webpack");
 const path = require("path");
 
 const config = {
-    entry: [
-        "react-hot-loader/patch",
-        "webpack-hot-middleware/client",
-        "./app/index",
-    ],
-    devServer: {inline: false},
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-      new webpack.DefinePlugin({
-        "process.env": {
-          NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-        },
-      })
-    ],
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "app.js",
-        publicPath: "/dist"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loaders: ["babel-loader"],
-                exclude: /node_modules/,
-                include: __dirname
-            },
-            {
-                test: /\.less$/,
-                loaders: [
-                    "style-loader",
-                    "css-loader",
-                    "less-loader"
-                ]
-            },
-            {
-                test: /\.css$/,
-                loaders:[
-                    "style-loader",
-                    "css-loader"
-                ]
-            },
-            {
-                test: /\.(otf|eot|png|svg|ttf|woff|woff2|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=8192"
-            }
+  entry: [
+    "react-hot-loader/patch",
+    "webpack-hot-middleware/client",
+    "./app/index",
+  ],
+  devServer: {inline: false},
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      },
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js",
+    publicPath: "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      {
+        test: /\.less$/,
+        loaders: [
+          "style-loader",
+          "css-loader",
+          "less-loader"
         ]
-    }
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(otf|eot|png|svg|ttf|woff|woff2|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=8192"
+      },
+      {
+        test: /\.(jpeg|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
+    ]
+  }
 };
 
 module.exports = config;
